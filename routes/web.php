4fdc4eff28559df6d938b9463d637d75ef3c40dd
genki <99254762+genki409@users.noreply.github.com>
2022-04-07 20:38:54 +0900

    ルーティング記載

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,33 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tops.top');
 });
+
+Route::get('/search', function () {
+    return view('tops.search');
+});
+Route::get('/search_result', 'SearchController@index')->name('searches.index');
+Route::get('/search_show/{id}', 'SearchController@show')->name('searches.show');
+
+Route::get('/records_show', function () {
+    return view('records.show');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/records', 'MemoController@index')->name('records.index')->middleware('auth');
+
+Route::get('/records_create', 'MemoController@create')->name('records.create')->middleware('auth');
+
+Route::post('/records', 'MemoController@store')->name('records.store');
+
+Route::get('/records/{id}', 'MemoController@show')->name('records.show')->middleware('auth');
+
+Route::get('/records/{id}/edit', 'MemoController@edit')->name('records.edit')->middleware('auth');
+
+Route::put('/records/{id}', 'MemoController@update')->name('records.update');
+
+Route::delete('/records/{id}', 'MemoController@destroy')->name('records.destroy');
