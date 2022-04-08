b5fab57c7103fb3574ced15e04647d3066ec4636
genki <99254762+genki409@users.noreply.github.com>
2022-04-08 19:44:40 +0900

    SearchController記述

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,5 +6,55 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $word = $request->word;
+        $base = $request->base_id;
+        $taste = $request->taste_id;
+        $alcohol = $request->alcohol;
+
+        if ($alcohol !== null) {
+            $alcohol = explode(" ", $alcohol);
+            $alcohol_from = (int) $alcohol[0];
+            $alcohol_to = (int) $alcohol[1];
+        } else {
+            $alcohol_from = '';
+            $alcohol_to = '';
+        }
+
+        $url = "https://cocktail-f.com/api/v1/cocktails?" . 'word=' . $word . '&' . 'base=' . $base . '&' . 'taste=' . $taste . '&' . 'alcohol_from=' . $alcohol_from . '&' . 'alcohol_to=' . $alcohol_to;
+        $method = "GET";
+
+        //接続
+        $client = new Client();
+
+        $response = $client->request($method, $url);
+
+        $posts = $response->getBody();
+        $posts = json_decode($posts, true);
+        $posts = $posts['cocktails'];
+        // $posts = array();
+        // dd($posts);
+
+        return view('tops.search_result', ['posts' => $posts]);
+        // return view('tops.search_result', compact('posts'));
+    }
+
+    public function show($id)
+    {
+        $url = "https://cocktail-f.com/api/v1/cocktails/" . $id;
+        $method = "GET";
+
+        //接続
+        $client = new Client();
+
+        $response = $client->request($method, $url);
+
+        $posts = $response->getBody();
+        $posts = json_decode($posts, true);
+        $posts = $posts['cocktail'];
+        // dd($posts);
+
+        return view('tops.search_show', ['posts' => $posts]);
+    }
 }
