4a2b36cdb409212c006dd4f21da82ffb34b43677
genki <99254762+genki409@users.noreply.github.com>
2022-05-17 17:09:36 +0900

    削除機能追加

--- resources/views/records/edit.blade.php
+++ resources/views/records/edit.blade.php
@@ -22,72 +22,76 @@
             </div>
             <div class="input-form-outside">
                 <div class="input-form">
-                    <form action="{{ route('records.update', $record->id) }}" method="POST">
-                        @csrf
-                        @method('put')
-                          <div class="form-top">
-                              <div class="top-left">
-                                  <div class="top-left-container">
-                                      <!-- 👇ここにプレビュー画像を追加する -->
-                                      <div class="upload-img">
-                                          <div id="preview"></div>
-                                      </div>
-                                      <label class="upload-label">
-                                          写真を選ぶ
-                                          <input type="file" id="example" name="image" multiple>
-                                      </label>
-                                  </div>
-                              </div>
-                              <div class="top-right">
-                                  <div class="top-right-container">
-                                      <div class="date">
-                                          <label for="start">日付</label>
-                                          <input type="date" id="today" name="date-form" >
-                                      </div>
-                                      <div class="name">
-                                          <label for="name">名前</label>
-                                          <input type="text" name="name" class="name-form" value="{{ $record->name }}">
-                                      </div>
-                                      <div class="base">
-                                          <label for="base">ベース</label>
-                                          <input type="text" name="base" class="base-form" value="{{ $record->base }}">
-                                      </div>
-                                      <div class="taste">
-                                          <label for="taste-form">味</label>
-                                          <input type="text" name="taste" class="taste-form" value="{{ $record->taste }}">
-                                      </div>
-                                      <div class="featuru">
-                                          <label for="featuru-form">特徴</label>
-                                          <textarea name="featuru" cols="30" rows="5">{{ $record->featuru }}</textarea>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                            <div class="form-middle">
-                                <div class="ingredient-top">
-                                    <h4>材料</h4>
+                    <div class="input-form-container">
+                        <form action="{{ route('records.update', $record->id) }}" method="POST">
+                            @csrf
+                            @method('put')
+                            <div class="form-top">
+                                <div class="top-left">
+                                    <div class="top-left-container">
+                                        <!-- 👇ここにプレビュー画像を追加する -->
+                                        <div class="upload-img">
+                                            <div id="preview"></div>
+                                        </div>
+                                        <label class="upload-label">
+                                            写真を選ぶ
+                                            <input type="file" id="example" name="image" multiple>
+                                        </label>
+                                    </div>
                                 </div>
-                                <div class="ingredient-text">
-                                  <textarea  name="ingredient" cols="110" rows="10" placeholder="材料や作り方など"></textarea>
+                                <div class="top-right">
+                                    <div class="top-right-container">
+                                        <div class="date">
+                                            <label for="start">日付</label>
+                                            <input type="date" id="today" name="date-form" >
+                                        </div>
+                                        <div class="name">
+                                            <label for="name">名前</label>
+                                            <input type="text" name="name" class="name-form" value="{{ $record->name }}">
+                                        </div>
+                                        <div class="base">
+                                            <label for="base">ベース</label>
+                                            <input type="text" name="base" class="base-form" value="{{ $record->base }}">
+                                        </div>
+                                        <div class="taste">
+                                            <label for="taste-form">味</label>
+                                            <input type="text" name="taste" class="taste-form" value="{{ $record->taste }}">
+                                        </div>
+                                        <div class="featuru">
+                                            <label for="featuru-form">特徴</label>
+                                            <textarea name="featuru" cols="30" rows="5">{{ $record->featuru }}</textarea>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                          
-                            <div class="form-bottom">
-                                <h4>コメント</h4>
-                                <div class="comment">
-                                    <textarea  name="comment" cols="110" rows="10">{{ $record->comment }}</textarea>
+                                <div class="form-middle">
+                                    <div class="ingredient-top">
+                                        <h4>材料</h4>
+                                    </div>
+                                    <div class="ingredient-text">
+                                    <textarea  name="ingredient" cols="110" rows="10" placeholder="材料や作り方など"></textarea>
+                                    </div>
                                 </div>
+                            
+                                <div class="form-bottom">
+                                    <h4>コメント</h4>
+                                    <div class="comment">
+                                        <textarea  name="comment" cols="110" rows="10">{{ $record->comment }}</textarea>
+                                    </div>
+                                </div>
+                            <!-- 登録ボタン -->
+                            <div class="register-derete">
+                                <button type="submit" class="register-btn">登録する</button>
                             </div>
-                        <!-- 登録ボタン -->
-                        <div class="register-derete">
-                            <button type="submit" class="register-btn">登録する</button>
-                            <form action='{{ route('records.destroy', $record->id) }}' method='post'>
-                                @csrf
-                                @method('delete')
-                                  <input type='submit' value='削除' class="derete-btn" onclick='return confirm("本当に削除しますか？");'>
-                            </form>
-                        </div>
-                      </form>
+                        </form>
+                    </div>
+                    <div class="derete-container">
+                        <form action='{{ route('records.destroy', $record->id) }}' method='post'>
+                            @csrf
+                            @method('delete')
+                            <input type='submit' value='削除' class="derete-btn" onclick='return confirm("本当に削除しますか？");'>
+                        </form>
+                    </div>
                 </div>
             </div>
             <div class="memory">
